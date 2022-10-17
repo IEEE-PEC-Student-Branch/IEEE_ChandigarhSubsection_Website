f345e2348b4d00a08d6e294412425e6ac911c9d3
Gaurav Kesarwani <gauravkesarwani@Gauravs-Mac-mini.local>
2022-10-17 08:41:20 -0700

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import subsectionlogo from './images/subsectionlogo.png';
+// import '.header.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav className="navbar" id="ftco-navbar">
+          <div className="container">
+            <a class="navbar-brand" href="index.html"><img src={subsectionlogo} width="150px" height="50px" /> </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="oi oi-menu"></span> Menu
+            </button>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active"><a href="index.html" className="nav-link">Home</a></li>
+              <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
+              <li className="nav-item"><a href="event.html" className="nav-link">Events</a></li>
+              <li className="nav-item"><a href="sponsors.html" className="nav-link">Sponsors</a></li>
+              <li className="nav-item"><a href="contact.html" className="nav-link">Contact Us</a></li>
+              <li className="nav-item"><a href="JoinIEEE.html" className="nav-link">Join IEEE</a></li>
+            </ul>
+          </div>
+        </nav>
     </div>
   );
 }
